bbbc1ef17e93ebe00ed8778e11a6a9376048df53
skeweredlogic <rjacobchick@gmail.com>
2014-10-02 22:36:17 -0400

    updated registration handler

--- app_server/Register.php
+++ app_server/Register.php
@@ -2,8 +2,27 @@
 
 class Register {
 	
-	public function post() {
-	
+	public function post($data) {
+		$success = false;
+
+		$user = $data['user'];
+		$pass = $data['pass'];
+		$type = isset($data['type']) ? $data['type'] : 'student';
+
+		$send = json_encode(array(
+			"user" => $user,
+			"pass" => $pass,
+			"type" => $type,
+			"cmd" => "register"));
+		$code = 0;
+		$result = json_decode(QueryDB($send, $code),true);
+		$success = $result['status'] === 'success';
+		if ($success){
+			echo "{\"message\":\"success\"}";
+		}else{
+			echo "{\"message\":\"failure\"}";
+		}
+
 	}
 }
 
