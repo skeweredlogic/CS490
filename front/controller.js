fe97d995e464a0aeb11e23bf7a5fe4fdcf1eabac
frostix2 <cjr29@njit.edu>
2014-10-21 09:38:41 -0400

    added multiple choice questions and added alert to login modal

--- front/controller.js
+++ front/controller.js
@@ -1,10 +1,6 @@
 /**
  * Created by frosti on 10/17/2014.
  */
-var request;
-var resp;
-
-
 function sendOver(command,data,callback){
 
     var sPackage;
@@ -16,7 +12,12 @@ function sendOver(command,data,callback){
     request = new XMLHttpRequest();
     request.open('POST', '../../~rj252/app_server/app.php', true);
 
-    request.onreadystatechange = callback;
+    request.onreadystatechange = function() {
+        if (request.status == 200 && request.readyState == 4){
+            resp = JSON.parse(request.responseText);
+            callback(resp);
+        }
+    };
 
     request.onerror = function() {
         console.log("request error");
@@ -34,30 +35,70 @@ function loginSend(){
     var password = document.getElementById('password').value;
     var credentials = {'user':username,'pass':password};
 
-    sendOver('login',credentials,function(){
-            if (request.status == 200 && request.readyState == 4){
-                resp = JSON.parse(request.responseText);
-                console.log(resp);
-                if (resp.backend==1){
-                    document.getElementById("alertBar").style.display="none";
-                    $('#loginModal').modal('hide');
-                }
-                else if (resp.backend==0){
-                  var alertBar = document.getElementById("alertBar");
-                    alertBar.className="alert alert-danger";
-                    alertBar.style.display="";
-                    alertBar.innerHTML="Invalid credentials";
-                    document.getElementById("password").parentNode.parentNode.appendChild(alertBar);
-                }
-
-                else if (resp.backend==-1){
-                    var alertBar = document.getElementById("alertBar")
-                    alertBar.className="alert alert-warning"
-                    alertBar.style.display="";
-                    alertBar.innerHTML="Connection error";
-                    document.getElementById("password").parentNode.parentNode.appendChild(alertBar);
-                }
+    sendOver('login',credentials,function(resp){
+            console.log(resp);
+            if (resp.backend==1){
+                document.getElementById("alertBar").style.display="none";
+                $('#loginModal').modal('hide');
             }
-    }
+            else if (resp.backend==0){
+                var alertBar = document.getElementById("alertBar");
+                alertBar.className="alert alert-danger";
+                alertBar.style.display="";
+                alertBar.innerHTML="Invalid credentials";
+                document.getElementById("password").parentNode.parentNode.appendChild(alertBar);
+            }
+
+            else if (resp.backend==-1){
+                var alertBar = document.getElementById("alertBar");
+                alertBar.className="alert alert-warning";
+                alertBar.style.display="";
+                alertBar.innerHTML="Connection error";
+                document.getElementById("password").parentNode.parentNode.appendChild(alertBar);
+            }
+        }
     );
+}
+
+function createMultipleChoice(){
+
+    var question = {
+        'type':'multi',
+        'question':document.getElementById("multiChoiceQuestion").value,
+        'answer':document.getElementById("multiChoiceAnswer").value,
+        'feedback':document.getElementById("multiChoiceAnswerFeedback").value,
+        'choice1':document.getElementById("multiChoice1").value,
+        'choice2':document.getElementById("multiChoice2").value,
+        'choice3':document.getElementById("multiChoice3".value)
+    };
+
+    sendOver('CreateQuestion',question,function(resp){
+            console.log(resp);
+    });
+}
+
+function createTrueFalse(){
+    var question = {
+        'type':'tf',
+        'question':document.getElementById("multiChoiceQuestion").value,
+        'answer':document.getElementById("multiChoiceAnswer").value,
+        'feedback':document.getElementById("multiChoiceAnswerFeedback").value
+
+    };
+    sendOver('CreateQuestion',question,function(resp){
+        console.log(resp);
+    });
+}
+
+function createCoding(){
+    var question ={
+        'type':'code',
+        'question':document.getElementById("multiChoiceQuestion").value,
+        'answer':document.getElementById("multiChoiceAnswer").value,
+        'feedback':document.getElementById("multiChoiceAnswerFeedback").value
+    };
+
+    sendOver('CreateQuestion',question,function(resp){
+        console.log(resp);
+    });
 }
