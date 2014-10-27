664d988b36a4da8378f75e03ca8837b4c3c4bfb7
skeweredlogic <rjacobchick@gmail.com>
2014-10-27 02:58:06 -0400

    removed some testing code that was commented out

--- app_server/GetBank.php
+++ app_server/GetBank.php
@@ -3,29 +3,6 @@ class GetBank {
 	
 	public function post($url) {
 		if (isset($_SESSION['uid']) && $_SESSION['login'] === true && $_SESSION['type'] === "instructor") {
-			/*$q1 = array(
-				"type" => "multi",
-				"question" => "yes?",
-				"answer" => "yes",
-				"feedback" => "obviously yes is the right answer",
-				"choice1" => "thing",
-				"choice2" => "stuff",
-				"choice3" => "asdf");
-			$q2 = array(
-				"type" => "tf",
-				"question" => "yes?",
-				"answer" => "true",
-				"feedback" => "obviously true");
-			$q3 = array(
-				"type" => "code",
-				"question" => "write some code yo",
-				"answer" => "1 2 3 4",
-				"feedback" => "you could do this with a simple for loop");
-			$data = array(
-				"0" => array("0" => $q1),
-				"1" => array("1" => $q2),
-				"2" => array("2" => $q3));
-			die(json_encode($data));*/
 
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $url);
--- app_server/GetExam.php
+++ app_server/GetExam.php
@@ -54,15 +54,17 @@ class GetExam {
 			$return = array();
 			$return[0] = array("eid" => $data['data']);
 
-			//if ($data['role'] === "student") {
+			if ($data['role'] === "student") {
 				$data['uid'] = $_SESSION['uid'];
 				$i = 1;
 				foreach ($exam as $key => $value) {
 					$return[$i] = $bank[$key];
-					//$return[$i][$key]['answered'] = isset($grades[$data['uid']]) ? $grades[$data['uid']] : -1;
 					$i++;
 				}
-			//}
+				if(isset($grades[$data['uid']])) {
+
+				}
+			}
 
 
 			die(json_encode($return));
@@ -75,95 +77,6 @@ class GetExam {
 				"status" => -1)));
 		}
 	}
-/*
-	private function getExam($data,$url) {
-		$data['cmd'] = "getExam";
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-		$gotExam = json_decode(curl_exec($ch),true);
-		$return_code = (string)curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-		curl_close($ch);
-
-		if (isset($gotExam['status'])) {
-			die(json_encode(array(
-				"status" => -1)));
-		}
-
-		return $gotExam;
-	}
-
-	private function getQuestions($data,$url) {
-		$data['cmd'] = "bank";
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-		$bank = json_decode(curl_exec($ch),true);
-		curl_close($ch);
-		if ($bank['status'] === -1) {
-			die(json_encode(array(
-				"status" => -1)));
-		}
-		return $bank;
-	}
-/*
-	private function getGrades($data,$url) {
-		$data['cmd'] = "getGrades";
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-		$gotGrades = json_decode(curl_exec($ch),true);
-		$return_code = (string)curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-		curl_close($ch);
-		if (isset($gotGrades['status'])) {
-			die(json_encode(array(
-				"status" => -1)));
-		}
-		return $gotGrades;
-	}
-
-	private function getStudentExam($data,$url) {
-		$data['cmd'] = "getStudentExam";
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-		$gotStudentExam = json_decode(curl_exec($ch),true);
-		$return_code = (string)curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-		curl_close($ch);
-
-		$success = $gotStudentExam['status'] === 1 && $return_code === "200";
-
-		if (!$success) {
-			die(json_encode(array(
-				"status" => -1,
-				"message" => "error getting student exam from backend")));
-		}
-		if ($success) {
-			return $gotStudentExam;
-		}
-		elseif ($return_code === "500") {
-			http_response_code(500);
-			die(json_encode(array(
-				"status" => -1,
-				"message" => "server error")));
-		}
-		else {
-			die(json_encode(array(
-				"status" => 0,
-				"message" => "Failed to retrieve exam")));
-		}
-	}*/
 
 }
 ?>
